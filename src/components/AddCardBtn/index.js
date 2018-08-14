697ef694817aeafa50c49541c5dd6e8d4ddcbbd7
Timur <lollipop.fly@gmail.com>
2018-08-14 15:27:11 +0300

    make home page

---
+++ src/components/AddCardBtn/index.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+import "./AddCardBtn.css";
+
+class AddCardBtn extends React.Component {
+	render() {
+		return <button className="add__card__btn">+</button>;
+	}
+}
+
+export default AddCardBtn;
